8ce04d993defee40fd64e7535e3d1b1ca4921cb0
Tavata Díaz <tavara.art@gmail.com>
2025-06-21 14:16:41 +0200

    multiidiom for menus retrieving from setting

--- menus/admin.py
+++ menus/admin.py
@@ -1,8 +1,15 @@
-# File: menus/admin.py
 from django.contrib import admin
+from django.conf import settings
 from mptt.admin import DraggableMPTTAdmin
 from modeltranslation.admin import TabbedTranslationAdmin
 from .models import Menu, MenuItem
+
+def get_translated_fieldnames(field_base):
+    """
+    Returns a tuple of field names for all languages defined in settings.LANGUAGES.
+    Example: ('title_en', 'title_es', 'title_ca', ...)
+    """
+    return tuple(f"{field_base}_{lang[0]}" for lang in settings.LANGUAGES)
 
 @admin.register(Menu)
 class MenuAdmin(TabbedTranslationAdmin):
@@ -11,9 +18,8 @@ class MenuAdmin(TabbedTranslationAdmin):
     It's kept simple, as the real management happens in MenuItemAdmin.
     """
     list_display = ('title', 'slug')
-    search_fields = ('title_en', 'title_es', 'title_ca')
-    prepopulated_fields = {'slug': ('title_en',)}
-
+    search_fields = get_translated_fieldnames('title')
+    prepopulated_fields = {'slug': (get_translated_fieldnames('title')[0],)}
 
 @admin.register(MenuItem)
 class MenuItemAdmin(DraggableMPTTAdmin, TabbedTranslationAdmin):
@@ -21,18 +27,11 @@ class MenuItemAdmin(DraggableMPTTAdmin, TabbedTranslationAdmin):
     Admin for the hierarchical MenuItem model.
     This provides a draggable, nested interface for easy menu management.
     """
-    # The fields to display in the tree view list
     list_display = (
-        'tree_actions',    # The drag handle and actions
-        'indented_title',  # The item's title, indented correctly
+        'tree_actions',
+        'indented_title',
         'link_type',
     )
-
-    # Make the main title clickable
     list_display_links = ('indented_title',)
-
-    # Allow filtering by the parent menu
     list_filter = ('menu',)
-
-    # MPTT-specific setting for the indentation width
     mptt_level_indent = 25
