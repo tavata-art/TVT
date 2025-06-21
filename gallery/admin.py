e7b2c79d395f8c32066f6aa6d2e21b6f5c272524
Tavata Díaz <tavara.art@gmail.com>
2025-06-21 12:00:19 +0200

    image admin adjusting by tabs using TabbedTranslationAdmin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import Image
 from django.utils.translation import gettext_lazy as _
+from modeltranslation.admin import TabbedTranslationAdmin
 
 @admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ('get_translated_title', 'uploaded_at', 'image')
+class ImageAdmin(TabbedTranslationAdmin):
+    list_display = (
+        'get_translated_title', 
+        'uploaded_at', 
+        'image'
+    )
 
     def get_translated_title(self, obj):
         return obj.title
--- gallery/models.py
+++ gallery/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Image(models.Model):
-    title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='gallery/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    # Title is translatable with django-modeltranslation
+    title = models.CharField(_("Title"), max_length=100)
+    image = models.ImageField(_("Image"), upload_to='gallery/')
+    uploaded_at = models.DateTimeField(_("Uploaded at"), auto_now_add=True)
 
     def __str__(self):
+        # Always return the translated title
         return self.title
